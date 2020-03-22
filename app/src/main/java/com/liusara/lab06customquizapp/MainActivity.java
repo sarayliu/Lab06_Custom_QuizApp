37e3d6df88eeea8f26fc6bc71a074fbf64606f65
Sara <sarayaoliu@gmail.com>
2020-03-22 02:11:49 -0400

    Lab06f Added TextChangedListener, Toasts, and addition checker; fixed ScrollView

--- app/src/main/java/com/liusara/lab06customquizapp/MainActivity.java
+++ app/src/main/java/com/liusara/lab06customquizapp/MainActivity.java
@@ -6,7 +6,6 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.os.Parcelable;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -43,8 +42,9 @@ public class MainActivity extends AppCompatActivity {
         nameText = findViewById(R.id.nameEditText);
         colorText = findViewById(R.id.colorEditText);
         hobbyText = findViewById(R.id.hobbyEditText);
-
         playButton = findViewById(R.id.playButton);
+        playButton.setEnabled(false);
+
         submitButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -62,6 +62,7 @@ public class MainActivity extends AppCompatActivity {
                 editor.apply();
                 System.out.println("Added " + gson.toJson(player));
                 System.out.println("After: " + sharedPreferences.getStringSet("storedPlayers", new HashSet<String>()));
+                playButton.setEnabled(true);
             }
         });
 
@@ -78,6 +79,7 @@ public class MainActivity extends AppCompatActivity {
                 }
                 playIntent.putExtra(PLAYER_INFO, playersArray);
                 startActivity(playIntent);
+                System.out.println("playButton clicked");
             }
         });
     }
--- app/src/main/java/com/liusara/lab06customquizapp/PlayLayout.java
+++ app/src/main/java/com/liusara/lab06customquizapp/PlayLayout.java
@@ -4,18 +4,30 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.gson.Gson;
 
+import java.util.Random;
+
 public class PlayLayout extends AppCompatActivity {
     Button homeButton;
+    TextView playersView;
     TextView questionTextView;
     EditText answerEditText;
-    Button submitButton;
+    Button submitAnswerButton;
+    Random rand = new Random();
+    int num1;
+    int num2;
+    int answer;
+    String displayString;
+    Toast toast;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,7 +44,7 @@ public class PlayLayout extends AppCompatActivity {
             playersInfo.append(gson.fromJson(playerInfo, Player.class)).append("\n\n");
         }
 
-        TextView playersView = findViewById(R.id.scoreboard);
+        playersView = findViewById(R.id.scoreboard);
         playersView.setText(playersInfo);
 
         homeButton = findViewById(R.id.homeButton);
@@ -41,16 +53,66 @@ public class PlayLayout extends AppCompatActivity {
             public void onClick(View v) {
                 Intent homeIntent = new Intent(getApplicationContext(), MainActivity.class);
                 startActivity(homeIntent);
+                System.out.println("Returned to Home");
             }
         });
 
         questionTextView = findViewById(R.id.questionTextView);
+        num1 = rand.nextInt(100);
+        num2 = rand.nextInt(100);
+        answer = num1 + num2;
+        displayString = num1 + " + " + num2;
+        questionTextView.setText(displayString);
         answerEditText = findViewById(R.id.answerEditText);
-        submitButton = findViewById(R.id.clickButton);
-        submitButton.setOnClickListener(new View.OnClickListener() {
+        submitAnswerButton = findViewById(R.id.submitAnswerButton);
+        submitAnswerButton.setEnabled(false);
+        System.out.println("submitAnswerButton disabled");
+        answerEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if(s.toString().equals(""))
+                {
+                    submitAnswerButton.setEnabled(false);
+                    System.out.println("submitAnswerButton disabled");
+                }
+                else
+                {
+                    submitAnswerButton.setEnabled(true);
+                    System.out.println("submitAnswerButton enabled");
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+        submitAnswerButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                if(answer == Integer.parseInt(answerEditText.getText().toString()))
+                {
+                    toast = Toast.makeText(getApplicationContext(), "Correct!", Toast.LENGTH_SHORT);
+                    toast.show();
+                }
+                else
+                {
+                    toast = Toast.makeText(getApplicationContext(), "Sorry, try again!", Toast.LENGTH_SHORT);
+                    toast.show();
+                }
                 answerEditText.setText("");
+                num1 = rand.nextInt(100);
+                num2 = rand.nextInt(100);
+                answer = num1 + num2;
+                displayString = num1 + " + " + num2;
+                questionTextView.setText(displayString);
+                System.out.println("submitAnswerButton clicked");
             }
         });
     }
